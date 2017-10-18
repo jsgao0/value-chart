11e821db10cfbf08e5b5c50ebb1a67e06de1368f
Anson <s1496k030@gmail.com>
2017-10-18 20:41:49 +0800

    add webpack minify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
-var path = require("path");
+var webpack = require("webpack"),
+    path = require("path");
+    
 module.exports = {
   entry: {
     app: ["./app/main.js"]
   },
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/assets/",
     filename: "bundle.js"
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+     minimize: true,
+     compress: false
+    })
+  ]
 };
